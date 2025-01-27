4b9cd4530635cbff57ee5e2403f1a1ace5081ebc
Your Name <your.email@example.com>
2025-01-27 20:25:37 +0530

    Your commit message here

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def decode_qr():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 2000))
     app.run(host="0.0.0.0", port=port)
 
 
